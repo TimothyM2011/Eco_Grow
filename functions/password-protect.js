4330a34a73e92ca7d40748931aab3ed4922f3ace
Timothy <timothymathew2011@gmail.com>
2024-10-31 11:25:11 +0400

    Updated password protection function with enhanced logging and path handling

--- functions/password-protect.js
+++ functions/password-protect.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async (event, context) => {
-  const password = "yourpassword"; // Set your password here
+  const password = "EcoGrow2024"; // Set your password here
   const authHeader = event.headers.authorization;
 
   console.log("Authorization Header:", authHeader); // Debugging line
 
   if (!authHeader) {
+    console.log("No authorization header present.");
     return {
       statusCode: 401,
       headers: {
@@ -24,13 +25,14 @@ exports.handler = async (event, context) => {
   console.log("Entered Password:", enteredPassword); // Debugging line
 
   if (enteredPassword !== password) {
+    console.log("Password does not match.");
     return {
       statusCode: 403,
       body: "Forbidden",
     };
   }
 
-  const publicPath = path.join(__dirname, '../..', 'public'); // Adjust this if your files are in a different location
+  const publicPath = path.join(__dirname, '../../', 'public'); // Adjust this if your files are in a different location
   const filePath = path.join(publicPath, event.path === '/' ? 'index.html' : event.path);
 
   console.log("File Path:", filePath); // Debugging line
